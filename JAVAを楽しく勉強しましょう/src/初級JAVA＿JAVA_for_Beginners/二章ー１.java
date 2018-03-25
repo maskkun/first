3dc9d8eca79a905edb83146838fed7be47a6e56d
Zeon <37477024+maskkun@users.noreply.github.com>
2018-03-25 11:35:58 +0900

    First commit

---
+++ JAVAを楽しく勉強しましょう/src/初級JAVA＿JAVA_for_Beginners/二章ー１.java
@@ -0,0 +1,21 @@
+package 初級JAVA＿JAVA_for_Beginners;
+
+public class 二章ー１ {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int x = 5;
+		double y = 0.114514;
+		System.out.println(x);
+		x = x+2;
+		System.out.println(x);
+		x += 2;
+		System.out.println(x);
+		x++;
+		System.out.println(x);
+		System.out.println(x+y);
+		
+	}
+
+}
---
+++ JAVAを楽しく勉強しましょう/src/初級JAVA＿JAVA_for_Beginners/二章ー２.java
@@ -0,0 +1,23 @@
+package 初級JAVA＿JAVA_for_Beginners;
+
+public class 二章ー２ {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		String message1 = "ずっと前からダイス………";
+		String message2 = "ケぇぇっぇぇぇっぇ";
+		System.out.println(message1);
+		System.out.println(message1 + message2);
+		
+		String tokuten = "得点＝";
+		int score = 10*30;
+		System.out.println(tokuten + score);
+		
+		boolean yes =true;
+		boolean no =false;
+		System.out.println("変数yesの値は" + yes + "です");
+		System.out.println("変数noの値は" + no + "です");
+	}
+
+}
